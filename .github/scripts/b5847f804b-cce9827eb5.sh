17966538e4d3df2e036482274d2924015fa6dbd4
sdarwin <samuel.d.darwin@gmail.com>
2021-01-05 01:48:19 +0000

    test

--- .github/scripts/b5847f804b-cce9827eb5.sh
+++ .github/scripts/b5847f804b-cce9827eb5.sh
@@ -3,7 +3,7 @@
 set -ex
 export TRAVIS_BUILD_DIR=$(pwd)
 export TRAVIS_BRANCH=${TRAVIS_BRANCH:-$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')}
-export TRAVIS_OS_NAME=${CI_JOB_OS_NAME:-linux}
+export TRAVIS_OS_NAME=${GHA_JOB_OS_NAME:-linux}
 export VCS_COMMIT_ID=$GITHUB_SHA
 export GIT_COMMIT=$GITHUB_SHA
 export DRONE_CURRENT_BUILD_DIR=$(pwd)
@@ -19,12 +19,12 @@ echo '==================================> INSTALL'
 git clone https://github.com/boostorg/boost-ci.git boost-ci
 cp -pr boost-ci/ci boost-ci/.codecov.yml .
 
-if [ "$DRONE_STAGE_OS" = "darwin" ]; then
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     unset -f cd
 fi
 
-export SELF=`basename $DRONE_REPO`
-export BOOST_CI_TARGET_BRANCH="$DRONE_COMMIT_BRANCH"
+export SELF=`basename $TRAVIS_BUILD_DIR`
+export BOOST_CI_TARGET_BRANCH="$TRAVIS_BRANCH"
 export BOOST_CI_SRC_FOLDER=$(pwd)
 
 . ./ci/common_install.sh
